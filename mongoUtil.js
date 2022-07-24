552f5ab85f633fa56f832e4e28c2821dd2c978be
Naythan Chan <48609695+naythan-c@users.noreply.github.com>
2022-07-23 22:27:16 -0700

    Update mongoUtil.js

--- mongoUtil.js
+++ mongoUtil.js
@@ -3,7 +3,6 @@ var _db;
 module.exports = {
   connectToServer:function(callback){
     const uri = process.env.mongo_uri
-    // const uri = "mongodb+srv://Parzival:bibi123@cluster0-9ekw7.mongodb.net/bibi-testing?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
     MongoClient.connect(uri,{useUnifiedTopology:true}, function(err, mongoClient) {
        if(err) {
             console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
